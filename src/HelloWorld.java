24a626ed0e4133817f31fbafc4a2eeebb40f7d67
Lucas-Pavao <89342683+Lucas-Pavao@users.noreply.github.com>
2022-05-21 14:29:07 -0300

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+	public static void main(String [] args){
+	System.out.println("Hello world");
+	int i = 0;
+	
+	int soma = i++;
+	
+	
+	System.out.println(soma);
+	}
+	
+	
+}
